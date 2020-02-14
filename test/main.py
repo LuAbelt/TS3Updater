7780eb635128f42954d64f0486c92851f679390e
Lukas Abelt <lukas.abelt@airbus.com>
2020-02-15 00:44:24 +0100

    -First working prototype

--- test/main.py
+++ test/main.py
@@ -2,8 +2,16 @@ import ts3updater.Version as updater
 from ts3updater.Updater import Updater
 
 
+def start():
+    print("Starting")
+
+
+def stop():
+    print("Stopping")
+
+
 def main():
-    up = Updater("/home/lukas/TS3", "/home/lukas/backup")
+    up = Updater("/home/lukas/TS3", stop, start, "/home/lukas/backup")
     up.perform_update(True, True)
 
 
--- ts3updater/Updater.py
+++ ts3updater/Updater.py
@@ -1,26 +1,33 @@
-import tempfile
-
 from ts3updater.Version import VersionChecker
+from typing import Callable
+import tarfile
+import tempfile
 import os
 import wget
 
 
 class Updater:
 
-    def __init__(self, install_dir: str, backup_dir: str = None):
+    def __init__(self, install_dir: str, shutdown_command: Callable, start_command: Callable, backup_dir: str = None):
         if type(install_dir) is not str:
             raise TypeError("install_dir must be of type str")
         if backup_dir is not None and type(backup_dir) is not str:
             raise TypeError("backup_dir must be of type str")
+        if not callable(shutdown_command):
+            raise TypeError("shutdown_command needs to be of type Callable, is of type " + type(shutdown_command))
+        if not callable(start_command):
+            raise TypeError("start_command needs to be of type Callable")
 
         if not os.path.isdir(install_dir):
             raise ValueError("install_dir is not a valid directory")
-
         if backup_dir is not None and not os.path.isdir(backup_dir):
-            raise ValueError("backup_dir is not a valid directory")
+            print("Specified backup_dir not existent yet. Creating...")
+            os.mkdir(backup_dir)
 
         self.install_dir = install_dir
         self.backup_dir = backup_dir
+        self.shutdown_sever = shutdown_command
+        self.start_server = start_command
         self.__DOWNLOAD_BASE_URL = "https://files.teamspeak-services.com/releases/server/%VERSION%/teamspeak3" \
                                    "-server_linux_amd64-%VERSION%.tar.bz2"
 
@@ -46,12 +53,32 @@ class Updater:
 
         download_url = self.__DOWNLOAD_BASE_URL.replace("%VERSION%", str(latest_version))
         print(download_url)
-        tmp_file = os.path.join(tempfile.gettempdir(), str(latest_version) + ".tar.bz2")
-        print(tmp_file)
+        tmp_file = str(latest_version) + ".tar.bz2"
         wget.download(download_url, tmp_file)
 
-        #Next steps:
-        # 1. Shutdown old server
-        # 2. Create Backup
-        # 3. Unpack new version
-        # 4. Start Server
+        self.shutdown_sever()
+
+        if create_backup:
+            backup_file = os.path.join(self.backup_dir, "TS3_{}_backup.tar.gz".format(str(cur_version)))
+            self.backup(backup_file)
+
+        print(self.install_dir)
+        print(tmp_file)
+        with tarfile.open(tmp_file, "r") as tar:
+            top_level_dir = tar.getmembers()[0].path+"/"
+            for member in tar.getmembers():
+                if member.path.startswith(top_level_dir):
+                    member.path = member.path[len(top_level_dir):]
+
+            tar.extractall(self.install_dir)
+
+        self.start_server()
+
+        os.remove(tmp_file)
+
+    def backup(self, backup_file: str):
+
+        with tarfile.open(backup_file, "w:gz") as tar:
+            for root, dirs, files in os.walk(self.install_dir):
+                for file in files:
+                    tar.add(os.path.join(root, file))
